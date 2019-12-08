42034801bc6e83be5751b3dd9f28d2e9ba5e48df
George Blackmore <gtblackmore@gmail.com>
2019-12-08 21:13:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,7 +12,10 @@ end
 def find_max_value(array)
   x = array[0]
   array.length.times { |index|
-  if array[index] >x
+  if array[index] > x
+    x = array[index]
+  }
+  x
 end
 
 def find_min_value(array)
